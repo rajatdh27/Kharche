9067521dbbed3b62d7f5099b148839006898195e
unionerror <rajatdh27@gmail.com>
2023-04-23 11:06:31 +0530

    add to home screen 3

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,40 @@
 /* eslint-disable no-restricted-globals */
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
+import { CacheFirst } from "workbox-strategies";
+import { ExpirationPlugin } from "workbox-expiration";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+
+// This should point to your precache manifest file.
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Here is an example of how to add a route to Workbox
+// to cache a specific API call and refresh it every 30 seconds.
+registerRoute(
+  /\/api\/my-api/,
+  new StaleWhileRevalidate({
+    cacheName: "my-api-cache",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 30,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
+);
+
+// Here is an example of how to add a route to Workbox
+// to cache a specific file (such as a logo) with a
+// CacheFirst strategy.
+registerRoute(
+  /\/logo.png/,
+  new CacheFirst({
+    cacheName: "logo-cache",
+  })
+);
 const CACHE_NAME = "kharche-cache";
 const urlsToCache = ["/", "/index.html"];
 
